c958b8f5d3579df59ab4b7ca32e7c6cea3718e08
gkarumba <gachegua@gmail.com>
2019-06-29 10:53:43 +0300

    Add the categories template

--- app/main/forms.py
+++ app/main/forms.py
@@ -3,7 +3,7 @@ from wtforms import StringField,TextAreaField,SubmitField
 from wtforms.validators import Required
 
 class PitchForm(FlaskForm):
-
+    category = SelectField('Type',choices=[('Business','Business'), ('Software','Software')],validators=[Required()])
     title = StringField('Pitch title',validators=[Required()])
     about = TextAreaField('About Pitch')
     submit = SubmitField('Submit')
@@ -11,3 +11,7 @@ class PitchForm(FlaskForm):
 class UpdateProfile(FlaskForm):
     bio = TextAreaField('Tell us about you.',validators = [Required()])
     submit = SubmitField('Submit')
+    
+class CommentsForm(FlaskForm):
+    comments = TextAreaField('Add your Comments', validators=[Required()])
+    submit = SubmitField('Submit Comment')
